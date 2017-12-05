53d178c072a8dbbdb14357002b5036d20e9163aa
Tom Brandon <tom.j.v.brandon@gmail.com>
2017-12-05 17:53:16 +0000

    Trying to complete the post_redirect_get pattern

--- app.rb
+++ app.rb
@@ -1,17 +1,27 @@
 require 'sinatra'
 
 
+
 class Battle < Sinatra::Base
+  enable :sessions
+
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params[:name_1]
-    @player_2 = params[:name_2]
-    erb(:names)
+    session[:name_1] = params[:name_1]
+    session[:name_2] = params[:name_2]
+    redirect '/play'
   end
   # run! if app_file == $0
+  get '/play' do
+    p session
+    @p1_name = session[:name_1]
+    @p2_name = session[:name_2]
+    erb(:play)
+  end
 end
 
 #app_file is the file being run. In this case app.rb
