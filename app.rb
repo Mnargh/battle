d85244adca5e73f38a803216ab8f29d6f8656491
James Lizamore <jwlizamore@gmail.com>
2017-12-05 16:25:55 +0000

    Added app and server config

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+  get '/' do
+    #p app_file
+    'Hello battle!'
+  end
+  p $0
+  p app_file
+  p "test"
+  run! if app_file == $0
+  p app_file
+end
+
+#app_file is the file being run. In this case app.rb
+# $0 Is the 1st positional param run in the shell
+# So if the first param
